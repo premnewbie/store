7936fa3c0a3b2e0e91fe6bd25d23cc95d45053f8
premnewbie <prempm50@gmail.com>
2024-10-10 21:54:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Assets/Logo.png'
+import searchicon from './Assets/search-normal.png'
+import heart from './Assets/heart.png'
+import profile from './Assets/profile.png'
+import shoppingbag from './Assets/shopping-bag.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} alt='logo' width={25} height={25}/>
+        <p className='logo-text'>LOGO</p>
+        <section className='header-icons'>
+          <img src={searchicon} alt='search-icon' width={20} height={20} />
+          <img src={heart} alt='hearct-icon' width={20} height={20} /> 
+          <img src={shoppingbag} alt='shopping-bag' width={20} height={20} />
+          <img src={profile} alt='profile' width={20} height={20} />
+          <p>Eng <i class="arrow-down"></i></p>
+        </section>
       </header>
     </div>
   );
